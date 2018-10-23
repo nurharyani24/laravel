7483cadab6245e21f205949d68c38a2401ffd8a0
Nur Haryani <nurharyani2407@gmail.com>
2018-10-23 15:25:41 +0700

    materi routing

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function () { // home
     return view('welcome');
+});
+
+Route::get('about', function () { // nama permalink
+    return view('about'); // nama file di folder views
+});
+
+Route::get('pesan', function () { // nama permalink
+    return view('pesan'); // nama file di folder views
+});
+
+Route::POST('pesan/kirim', function () { // nama permalink. Metodnya diganti karena kita mengambil data dengan method POST
+    return view('pesan_kirim'); // nama file di folder views yang akan diakses
 });
